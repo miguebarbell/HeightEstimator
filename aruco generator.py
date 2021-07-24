64dca507565046442b924461c71d8a8d163796dc
Migue <migue@tuta.io>
2021-07-23 21:57:52 -0400

    adding files

---
+++ aruco generator.py
@@ -0,0 +1,33 @@
+import cv2
+import numpy as np
+
+
+# generate aruco dict
+aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_ARUCO_ORIGINAL)
+
+# generate the space 300x300
+tag = np.zeros((300, 300, 1), dtype="uint8")
+
+# draw the tag id=8 in the space
+"""
+The first parameter is the Dictionary object previously created.
+The second parameter is the marker id, in this case the marker 68 of the dictionary DICT_ARUCO_ORIGINAL. 
+The third parameter, 300, is the size of the output marker image (300x300). So, for instance, you cannot generate an image of 5x5 pixels for a marker size of 6x6 bits.
+The fourth parameter is the output image.
+Finally, the last parameter is an optional parameter to specify the width (internal bits) of the marker black border. The default value is 1.
+"""
+cv2.aruco.drawMarker(aruco_dict, 68, 300, tag, 1)
+
+# show the image
+cv2.imshow('tag', tag)
+cv2.waitKey(0)
+
+# ask for saving the image
+output = "tag.jpg"
+print('Save the image? [Y]/n')
+answer = input()
+if answer != 'n':
+    cv2.imwrite(f'images/{output}', tag)
+    print(f'Tag saved as {output}')
+else:
+    print('Tag not saved, quitting...')
---
+++ height_estimator.py
@@ -0,0 +1,60 @@
+import cv2
+import imutils
+import numpy as np
+from datetime import datetime
+
+# import image
+input_image = "images/mads3.jpg"
+image = cv2.imread(input_image)
+image = imutils.resize(image, width=1200)
+# detect aruco tag
+aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_ARUCO_ORIGINAL)
+aruco_params = cv2.aruco.DetectorParameters_create()
+(corners, aruco_id, rejected) = cv2.aruco.detectMarkers(image, aruco_dict, parameters=aruco_params)
+if aruco_id != None:
+    print(f"[INFO] Found aruco tag id: {np.squeeze(aruco_id)}")
+    corners = np.squeeze(corners[0])
+    lowest_point = int(max(corners[2:4, 1]))
+    # print(f"[INFO] Lowest point: {lowest_point}")
+    # cv2.line(image, (610, lowest_point), (529, lowest_point), (0, 255, 0), 3)
+    # calcular la altura del tag
+    tag_height = corners[2, 1] - corners[1, 1]
+
+# detect face
+cascade = "../../commonfiles/haarcascade_frontalface_default.xml"
+# cascade = "../../commonfiles/haarcascade_fullbody.xml"
+detector = cv2.CascadeClassifier(cascade)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+print("[INFO] performing Human detection...")
+rects = detector.detectMultiScale(gray, scaleFactor=1.2,
+                                  minNeighbors=5, minSize=(30, 30),
+                                  flags=cv2.CASCADE_SCALE_IMAGE)
+print("[INFO] {} Human detected...".format(len(rects)))
+
+# line parameters
+thickness = 1
+line_color = (0, 255, 0)
+# loop over the bounding boxes
+for (x, higher_point, w, h) in rects:
+    # draw the line of the total height
+    cv2.rectangle(image, (x+w+6, higher_point-13), (x+w+95, higher_point+3), (0, 0, 0), -1)
+    cv2.line(image, (x+w+5, lowest_point), (x+w+5, higher_point), line_color, thickness)
+    cv2.line(image, (x+w+5, lowest_point), (x+w-10, lowest_point), line_color, thickness)
+    cv2.line(image, (x+w+5, higher_point), (x+w-10, higher_point), line_color, thickness)
+
+    # compute distances
+    human_height = (((lowest_point - higher_point)*19)/tag_height) + 5
+    cv2.putText(image, f"{str(round(human_height, 1))} cms", (x+w+7, higher_point), cv2.FONT_HERSHEY_SIMPLEX, 0.5, line_color, thickness)
+
+# Syntax: cv2.putText(image, text, org, font, fontScale, color[, thickness[, lineType[, bottomLeftOrigin]]])
+
+# draw the image using the time
+cv2.imshow('test', image)
+cv2.waitKey(0)
+
+# save the result
+# print("Do you wan to save the image? [Y]/n")
+answer = input("Do you wan to save the image? [Y]/n\n")
+if answer.lower() != 'n' or answer.lower() != 'no':
+    output = f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}.jpg"
+    cv2.imwrite(output, image)
